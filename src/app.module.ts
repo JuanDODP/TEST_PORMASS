3c3395d02bee1d9b4eb6f18bd3040b8713b99142
Juan Dominguez <juan.dominguez.dp@gmail.com>
2026-06-20 20:00:59 -0600

    init: initial project setup with NestJS and doctors module

---
+++ src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { DoctorsModule } from './doctors/doctors.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true, // No usar en producción
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+    DoctorsModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule { }
